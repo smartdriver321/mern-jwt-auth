4e244d0e16ac0e38c5b894494171cad7ded5bf89
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-07 11:16:05 +0700

    Configure Tanstack Query

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+import queryClient from './config/queryClient.js'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+	<QueryClientProvider client={queryClient}>
+		<BrowserRouter>
+			<App />
+			<ReactQueryDevtools position='bottom-right' initialIsOpen={false} />
+		</BrowserRouter>
+	</QueryClientProvider>
 )

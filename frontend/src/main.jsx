98b609a97e1c465ce4f11a549d6a34e388bd0110
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-07 11:21:40 +0700

    Configure Chakra UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,20 @@
 import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
+import theme from './theme/index.js'
 import queryClient from './config/queryClient.js'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<QueryClientProvider client={queryClient}>
-		<BrowserRouter>
-			<App />
-			<ReactQueryDevtools position='bottom-right' initialIsOpen={false} />
-		</BrowserRouter>
-	</QueryClientProvider>
+	<ChakraProvider theme={theme}>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<App />
+				<ReactQueryDevtools position='bottom-right' initialIsOpen={false} />
+			</BrowserRouter>
+		</QueryClientProvider>
+	</ChakraProvider>
 )

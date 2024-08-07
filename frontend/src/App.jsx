b310cc176a410a835e9ee1300f79ccf0bcbee80d
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-07 15:21:31 +0700

    Create ResetPassword page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Register from './pages/Register'
 import Login from './pages/Login'
 import VerifyEmail from './pages/VerifyEmail'
 import ForgotPassword from './pages/ForgotPassword'
+import ResetPassword from './pages/ResetPassword'
 
 export const Home = () => {
 	return <div>Home</div>
@@ -17,6 +18,7 @@ function App() {
 			<Route path='/login' element={<Login />} />
 			<Route path='/email/verify/:code' element={<VerifyEmail />} />
 			<Route path='/password/forgot' element={<ForgotPassword />} />
+			<Route path='/password/reset' element={<ResetPassword />} />
 		</Routes>
 	)
 }
